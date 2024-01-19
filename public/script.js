696c6686acc6911b9e7b6ed51d2bf4248ea9d35c
Jesse Foley <jessefoley10@yahoo.com>
2024-01-18 20:42:03 -0600

    work on creating the punishment modal

--- public/script.js
+++ public/script.js
@@ -9,7 +9,6 @@ const fetchActivity = (year) => {
     })
     .then (response => response.json())
     .then (data => {
-        console.log(data);
         populateActivity(data);
     })
     .catch (error => {
@@ -55,12 +54,11 @@ const changeYear = (year) => {
         case 1:
             changeYearColor('/images/salty-logos/png/logo-no-background.png', 'rgb(33, 33, 33)', 
                             'rgb(52, 52, 52)', 'rgb(235, 235, 235)');
-            changeLeagueType('Classic', 'classic-league', '/images/icons/american-football.png');
+            //changeLeagueType('Classic', 'classic-league', '/images/icons/american-football.png');
             break;
         case 2:
             changeYearColor('/images/salty-logos/png/salty-high-resolution-logo-white-transparent (1).png', 
                             '#551010', '#671E1E', 'rgb(235, 235, 235)');
-            changeLeagueType('Vampire', 'vampire-league', '/images/icons/vampire.png');
             break;
         case 3:
             changeYearColor('/images/salty-logos/png/logo-no-background.png', 
@@ -217,21 +215,36 @@ const createCategoryContainer = (rules, url) => {
 
 // League Types Rules
 
-document.querySelector('.classic-league').addEventListener('click', () => {
-    const categoriesContainer = document.createElement('div');
-    categoriesContainer.classList.add('categories-container');
+if(document.querySelector('.classic-league')) {
+    document.querySelector('.classic-league').addEventListener('click', () => {
+        const categoriesContainer = document.createElement('div');
+        categoriesContainer.classList.add('categories-container');
+    
+    
+        const activityContainer = document.querySelector('.activity-container');
+        activityContainer.innerHTML = '';
+    
+        let description = document.createElement('p');
+        description.classList.add('description-text');
+    
+        description.innerHTML = 'Classic scoring rules. The six teams with the best record make the playoffs.';
+    
+        activityContainer.appendChild(description);
+        activityContainer.appendChild(categoriesContainer);
+    
+        categoriesContainer.appendChild(createCategoryContainer('Win a game (+5)', 'images/icons/trophy.png', currentYear));
+        categoriesContainer.appendChild(createCategoryContainer('Score above the median (+3)', 'images/icons/average.png', currentYear));
+        categoriesContainer.appendChild(createCategoryContainer('Highest Scorer (+8)', 'images/icons/number-one.png', currentYear));
+    
+    });
+}
 
+document.querySelector('.punishment-title').addEventListener('click', openModal);
 
-    const activityContainer = document.querySelector('.activity-container');
-    activityContainer.innerHTML = '';
+function openModal() {
+  document.querySelector('.punishment-modal').style.display = 'block';
+}
 
-    let description = document.createElement('p');
-    description.classList.add('description-text');
-
-    description.innerHTML = 'Classic scoring rules. The six teams with the best record make the playoffs.';
-
-    activityContainer.appendChild(description);
-    activityContainer.appendChild(categoriesContainer);
-
-    categoriesContainer.appendChild(createCategoryContainer('Win a game (+5)', 'images/icons/trophy.png', currentYear));
-});
+function closeModal() {
+  document.querySelector('.punishment-modal').style.display = 'none';
+}
